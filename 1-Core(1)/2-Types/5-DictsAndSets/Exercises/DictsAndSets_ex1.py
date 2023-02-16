39d0bb99f56f5505b22350b08e8a5ddbae741e55
Oliver Huo <oliverhuo128@gmail.com>
2023-02-16 17:24:00 +0000

    helloo there

--- 1-Core(1)/2-Types/5-DictsAndSets/Exercises/DictsAndSets_ex1.py
+++ 1-Core(1)/2-Types/5-DictsAndSets/Exercises/DictsAndSets_ex1.py
@@ -1,5 +1,23 @@
 #enter your cleanup_address_book function here:
+# keys are the names, values are the numbers 'stored as strings' 
+x = {'oliver huo': '07575218700', 'Dad': 447723351072, 'mum and sister': 447795513264, 'here is  na random number':348902574763095476354097630548764}
+
 
 def cleanup_address_book(address_book):
-    pass
+    cleaned_address_book = {}
+    for name in address_book:
+        new_name = name.title()
+        new_number = str(address_book[name])
+        if len(new_number) > 7:
+            new_new_number = new_number[0:-6] + " " + new_number[-6:]
+        else:
+            new_new_number = new_number
+        cleaned_address_book[new_name] = new_new_number
+
+
+    return cleaned_address_book    
+
+print(cleanup_address_book(x))
+
+
 
